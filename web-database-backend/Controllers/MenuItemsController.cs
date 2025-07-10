1b986b139d8190a421d057c0d8db1571b9c594c9
Myslitel666 <tretyakov_02@bk.ru>
2025-07-10 17:39:32 +0500

    Added MenuItemsController

---
+++ web-database-backend/Controllers/MenuItemsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web_database_backend.Context;
+using web_database_backend.Models;
+
+namespace web_database_backend.Controllers
+{
+    [Route("api/menuItems")]
+    [ApiController]
+    public class MenuItemsController : ControllerBase
+    {
+        private readonly WebDatabaseContext _webDatabaseContext;
+
+        public MenuItemsController(WebDatabaseContext webDatabaseContext)
+        {
+            _webDatabaseContext = webDatabaseContext;
+        }
+
+        // GET: api/menuItems/getParentMenuItems
+        [HttpGet("getParentMenuItems")]
+        public async Task<ActionResult<IEnumerable<MenuItem>>> GetParentMenuItems()
+        {
+            return await _webDatabaseContext.MenuItems
+                .Where(item => item.ParentId == null)
+                .OrderBy(item => item.Order) // Дополнительная сортировка по полю Order
+                .ToListAsync();
+        }
+    }
+}
