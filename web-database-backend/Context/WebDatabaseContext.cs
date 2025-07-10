f0c380b3338f523fc1e87c8a7d1de18711edcc7d
Myslitel666 <tretyakov_02@bk.ru>
2025-07-10 10:51:59 +0500

    Added the database context and model classes.

---
+++ web-database-backend/Context/WebDatabaseContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using web_database_backend.Models;
+
+namespace web_database_backend.Context;
+
+public partial class WebDatabaseContext : DbContext
+{
+    public WebDatabaseContext()
+    {
+    }
+
+    public WebDatabaseContext(DbContextOptions<WebDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<MenuItem> MenuItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=WebDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MenuItem>(entity =>
+        {
+            entity.Property(e => e.Order).HasColumnName("order");
+            entity.Property(e => e.ParentId).HasColumnName("parentId");
+            entity.Property(e => e.Title)
+                .HasMaxLength(128)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.Url)
+                .HasMaxLength(256)
+                .IsUnicode(false)
+                .HasColumnName("url");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ web-database-backend/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace web_database_backend.Models;
+
+public partial class MenuItem
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Url { get; set; }
+
+    public int? ParentId { get; set; }
+
+    public int Order { get; set; }
+}
